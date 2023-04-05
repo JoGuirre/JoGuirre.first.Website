ed44f036c7060333dc06a8464151afbca0df15be
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-05 14:35:11 -0700

    added mui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,19 @@ import '../css/Header.css'
 function Header() {
   return (
     <div className="header__container">
-        Header
+        <div className="header__logo">
+            Logo
+        </div>
+        <div className="header__space">
+            blank space
+        </div>
+        <div className="nav__links">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
     </div>
   )
 }
