8b11c0642e8af0d19a480a490dbfb6365405d83b
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-05 15:13:46 -0700

    this sidebar is not working!!!!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import '../css/Header.css'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ContactPageOutlinedIcon from '@mui/icons-material/ContactPageOutlined';
+import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
 
 function Header() {
   return (
@@ -7,14 +10,12 @@ function Header() {
         <div className="header__logo">
             Logo
         </div>
-        <div className="header__space">
-            blank space
-        </div>
+        <div className="header__space"></div>
         <div className="nav__links">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href=""><HomeOutlinedIcon className="nav__icon"/><span className="nav__title">Home</span></a></li>
+                <li><a href=""><LiveHelpOutlinedIcon className="nav__icon"/><span className="nav__title">About</span></a></li>
+                <li><a href=""><ContactPageOutlinedIcon className="nav__icon"/><span className="nav__title">Contact</span></a></li>
             </ul>
         </div>
     </div>
