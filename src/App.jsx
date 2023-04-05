15ab80dee8110840abbf79770ce7f09241aeac11
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-05 13:32:50 -0700

    base project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className="app">
       App
     </div>
   )
