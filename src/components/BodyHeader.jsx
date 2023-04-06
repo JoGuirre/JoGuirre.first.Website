5490d5c2971cb01619544df260bb6c8f9c44b8e6
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-06 13:01:29 -0700

    mmmm im not sure what happened in this commit but i have modified files from yesterday

--- src/components/BodyHeader.jsx
+++ src/components/BodyHeader.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import '../css/BodyHeader.css'
 
 function BodyHeader() {
+  const handleClick = () => {
+    console.log('Contact Me Clicked');
+  }
   return (
     <div className="body__container">
         <div className="body__intro">
             <h1 className="body__intro__header">Hi,<br/>I'm Josh,<br/>web developer<br/></h1>
             <h3 className="body__intro__subtext">Front-End | Back-End | Full Stack<br/><span id="intro__dev">Developer</span></h3>
-            <button className="body__intro__button">Contact Me</button>
+            <button onClick={handleClick} className="body__intro__button">Contact Me</button>
         </div>
     </div>
   )
