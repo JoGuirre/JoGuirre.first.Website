99aaa1deef9117a80bf972b80e5685a5321d1cfa
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-05 17:03:04 -0700

    refactored body header to its own component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import '../css/Body.css'
+import BodyHeader from './BodyHeader'
 
 function Body() {
   return (
-    <div className="body__container">
-        Body
-    </div>
+    // <div className="body__container">
+    //     <div className="body__intro">
+    //         <h1 className="body__intro__header">Hi,<br/>I'm Josh,<br/>web developer<br/></h1>
+    //         <h3 className="body__intro__subtext">Front-End | Back-End | Full Stack<br/><span id="intro__dev">Developer</span></h3>
+    //         <button className="body__intro__button">Contact Me</button>
+    //     </div>
+    // </div>
+    <BodyHeader />
   )
 }
 
