8bb7e93b3711a9b05e979d6aa2c938108cad3bd9
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-13 15:26:29 -0700

    email stuff and a popup stuff

--- src/components/JoinedPopup.jsx
+++ src/components/JoinedPopup.jsx
@@ -5,7 +5,13 @@ function JoinedPopup() {
   return (
     <div className="popup__container">
       <div className="things">
-        <h1>Thanks!</h1>
+        <h1>Your email has been sent!</h1>
+        <p>
+          Thank you for your message!
+          <br />I will be in contact within 2 business days.
+          <br />
+          Until then, go flub urself beotch.
+        </p>
       </div>
     </div>
   );
