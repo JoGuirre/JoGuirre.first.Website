eb289054ab613819b3ddc43795cac73198e24dc1
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-24 11:57:38 -0700

    blobs n shizz

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import '../css/Body.css'
-import BodyHeader from './BodyHeader'
-import AboutMe from './AboutMe'
-import Projects from './Projects'
-import ContactMe from './ContactMe'
+import React from "react";
+import "../css/Body.css";
+import BodyHeader from "./BodyHeader";
+import AboutMe from "./AboutMe";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+import BlobArt from "../assets/circle-scatter-haikei.svg";
 
 function Body() {
   return (
     <div className="body__container">
-        <BodyHeader />
-        <AboutMe />
-        <Projects />
-        <ContactMe />
+      <BodyHeader />
+      <AboutMe />
+      <Projects />
+      <ContactMe />
+      <div className="background__blob__container">
+        {/* <img className="background__blob" src={BlobArt} alt="Blobs" /> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
